96de4c23c82b52eb8b597f4789b1ae7d401eca4c
AntonTakashvili <anton.takashvili.1@btu.edu.ge>
2023-01-04 14:27:56 +0300

    done

---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Mssqlconnection
+{
+    public partial class Form1 : Form
+    {
+        public string fname, lname, phone;
+        int id;
+        DataTable table = new DataTable();
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-9BL9P4H\MSSQLSERVER2022;
+                Initial Catalog=TestDb;
+                Integrated Security=True;");
+
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+            GetTable();
+        }
+        //Fill datagrid by taking all information from DB
+        public void GetTable()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT *FROM dbo.guest3",conn);
+            table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+        //insert function
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string sqlQuery = "INSERT INTO dbo.guest3 VALUES" +
+                "('" + textBox1.Text+ "','" + textBox2.Text + "','" + textBox3.Text + "')";
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(sqlQuery, conn);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("insert Successful", "Success");
+                conn.Close();
+                GetTable();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //Clear Button
+        private void clearbtn_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+
+        //Contnet click In dataGridViewer
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //if (e.RowIndex == 0)
+            //    dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            //textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            //textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            //textBox3.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+        }
+
+        //Edit in MSSQL DATABASE
+        private void updatebtn_Click(object sender, EventArgs e)
+        {
+            string editQuery = 
+                "update dbo.guest3 set FirstName='"+textBox1.Text+"',LastName='"+textBox2.Text+"', Phone='"+textBox3.Text+"' where GuestID='"+id+"'";
+
+
+            try
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(editQuery, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Update Successful", "Success");
+                conn.Close();
+                GetTable();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //Delete btn
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            string deleteQuery =
+                "delete from dbo.guest3 where GuestID='" + id + "'";
+            try
+            {
+                conn.Open();
+                SqlCommand cmd= new SqlCommand(deleteQuery, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Deleted Successfully", "Success");
+                conn.Close();
+                GetTable();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+       
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Mssqlconnection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mssqlconnection")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f07bdce4-3bad-461c-af6c-792e9611c62a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
